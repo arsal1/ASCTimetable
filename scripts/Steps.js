99911983edd833f27d93753585308f19fee1505b
muhammad_arsal <arsal_bscs@yahoo.com>
2015-08-13 01:00:08 +0500

    Data Fetch

--- scripts/Steps.js
+++ scripts/Steps.js
@@ -5,7 +5,7 @@ function $id(myId) {
     return document.getElementById(myId);
 }
 
-function $changeImage(imgID){
+function $changeImage(imgID) {
     return document.getElementById(imgID).src = "../images/Steps/no_32.png";
 }
 
@@ -15,7 +15,7 @@ function $class(myClass) {
 
 var StepsModule = {
     step1: {
-        goFurther: function() {/*
+        goFurther: function () {/*
          this.checkButton = $('#chkAgree');
          if(this.checkButton.is(':checked')) {
          this.btnTerms.prop('disabled', false);
@@ -67,22 +67,22 @@ function goFurther() {
 
 //Step 2
 
-var addClassToShowImageOnly = function(elem){
+var addClassToShowImageOnly = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
 
-    var result = [].filter.call(allChildNodes, function(items, index){
+    var result = [].filter.call(allChildNodes, function (items, index) {
 
         return /^IMG/.test(items.nodeName);
     });
-    result.forEach(function(item){
-        item.className= "dont-show";
+    result.forEach(function (item) {
+        item.className = "dont-show";
     })
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected = function(elem){
+var addClassToShowSelected = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
-    if (elem && elem.innerHTML == "No"){
+    if (elem && elem.innerHTML == "No") {
 
         document.getElementById('textshow').innerHTML = 'Please Check this help link';
     } else {
@@ -90,17 +90,17 @@ var addClassToShowSelected = function(elem){
 
     }
 
-    var result = [].filter.call(allChildNodes, function(items, index){
+    var result = [].filter.call(allChildNodes, function (items, index) {
 
         return /^IMG/.test(items.nodeName);
     });
-    result.forEach(function(item){
-        item.className= "dont-show";
+    result.forEach(function (item) {
+        item.className = "dont-show";
     })
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected3 = function(elem) {
+var addClassToShowSelected3 = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
     if (elem && elem.innerHTML == "Yes") {
 
@@ -120,28 +120,28 @@ var addClassToShowSelected3 = function(elem) {
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected4 = function(elem){
+var addClassToShowSelected4 = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
-    if (elem && elem.innerHTML == "Yes"){
+    if (elem && elem.innerHTML == "Yes") {
         document.getElementById('textshow3').innerHTML = 'Please Check this help link';
     } else if (elem && elem.innerHTML == "Not Sure") {
         document.getElementById('textshow3').innerHTML = 'Please Check this help link';
     }
-    else{
+    else {
         document.getElementById('textshow3').innerHTML = '';
     }
 
-    var result = [].filter.call(allChildNodes, function(items, index){
+    var result = [].filter.call(allChildNodes, function (items, index) {
 
         return /^IMG/.test(items.nodeName);
     });
-    result.forEach(function(item){
-        item.className= "dont-show";
+    result.forEach(function (item) {
+        item.className = "dont-show";
     })
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected5 = function(elem) {
+var addClassToShowSelected5 = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
     if (elem && elem.innerHTML == "Yes") {
 
@@ -161,7 +161,7 @@ var addClassToShowSelected5 = function(elem) {
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected6 = function(elem) {
+var addClassToShowSelected6 = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
     if (elem && elem.innerHTML == "Yes") {
 
@@ -181,7 +181,7 @@ var addClassToShowSelected6 = function(elem) {
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected7 = function(elem) {
+var addClassToShowSelected7 = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
     if (elem && elem.innerHTML == "Yes") {
 
@@ -201,7 +201,7 @@ var addClassToShowSelected7 = function(elem) {
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected8 = function(elem) {
+var addClassToShowSelected8 = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
     if (elem && elem.innerHTML == "Yes") {
 
@@ -221,7 +221,7 @@ var addClassToShowSelected8 = function(elem) {
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected9 = function(elem) {
+var addClassToShowSelected9 = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
     if (elem && elem.innerHTML == "Yes") {
 
@@ -241,7 +241,7 @@ var addClassToShowSelected9 = function(elem) {
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected10 = function(elem) {
+var addClassToShowSelected10 = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
     if (elem && elem.innerHTML == "Yes") {
 
@@ -261,7 +261,7 @@ var addClassToShowSelected10 = function(elem) {
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected11 = function(elem) {
+var addClassToShowSelected11 = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
     if (elem && elem.innerHTML == "Yes") {
 
@@ -281,7 +281,7 @@ var addClassToShowSelected11 = function(elem) {
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected12 = function(elem) {
+var addClassToShowSelected12 = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
     if (elem && elem.innerHTML == "Yes") {
 
@@ -301,7 +301,7 @@ var addClassToShowSelected12 = function(elem) {
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected13 = function(elem) {
+var addClassToShowSelected13 = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
     if (elem && elem.innerHTML == "Yes") {
 
@@ -321,28 +321,28 @@ var addClassToShowSelected13 = function(elem) {
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected14 = function(elem){
+var addClassToShowSelected14 = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
-    if (elem && elem.innerHTML == "Yes"){
+    if (elem && elem.innerHTML == "Yes") {
         document.getElementById('textshow13').innerHTML = 'Click here to enable 0th period';
     } else if (elem && elem.innerHTML == "Not Sure") {
         document.getElementById('textshow13').innerHTML = 'Click here to enable or disable 0th period';
     }
-    else{
+    else {
         document.getElementById('textshow13').innerHTML = '';
     }
 
-    var result = [].filter.call(allChildNodes, function(items, index){
+    var result = [].filter.call(allChildNodes, function (items, index) {
 
         return /^IMG/.test(items.nodeName);
     });
-    result.forEach(function(item){
-        item.className= "dont-show";
+    result.forEach(function (item) {
+        item.className = "dont-show";
     })
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected15 = function(elem) {
+var addClassToShowSelected15 = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
     if (elem && elem.innerHTML == "Yes") {
 
@@ -362,23 +362,23 @@ var addClassToShowSelected15 = function(elem) {
     elem.previousElementSibling.className = "";
 }
 
-var addClassToShowSelected16 = function(elem){
+var addClassToShowSelected16 = function (elem) {
     var allChildNodes = elem.parentNode.childNodes;
-    if (elem && elem.innerHTML == "Yes"){
+    if (elem && elem.innerHTML == "Yes") {
         document.getElementById('textshow15').innerHTML = 'Please Check this help link';
     } else if (elem && elem.innerHTML == "Not Sure") {
         document.getElementById('textshow15').innerHTML = 'Please Check this help link';
     }
-    else{
+    else {
         document.getElementById('textshow15').innerHTML = '';
     }
 
-    var result = [].filter.call(allChildNodes, function(items, index){
+    var result = [].filter.call(allChildNodes, function (items, index) {
 
         return /^IMG/.test(items.nodeName);
     });
-    result.forEach(function(item){
-        item.className= "dont-show";
+    result.forEach(function (item) {
+        item.className = "dont-show";
     })
     elem.previousElementSibling.className = "";
 }
@@ -387,7 +387,6 @@ var addClassToShowSelected16 = function(elem){
 
 
 /** Step3 **/
-
 
 
 /**Step4**/
@@ -399,20 +398,20 @@ function Constraint() {
         $id("show").disabled = true;
     }
 }
-    function ConstraintLunch() {
-        if ($id("Lunch").checked == true) {
-            $id("Lunch1").disabled = false;
-            $id("Lunch2").disabled = false;
-            $id("chkLunch").disabled = false;
-        }
-        else {
-            $id("Lunch1").disabled = true;
-            $id("Lunch2").disabled = true;
-            $id("chkLunch").disabled = true;
-        }
+function ConstraintLunch() {
+    if ($id("Lunch").checked == true) {
+        $id("Lunch1").disabled = false;
+        $id("Lunch2").disabled = false;
+        $id("chkLunch").disabled = false;
     }
-function LoadChange(){
-    if ($id("chkClassTeacher").checked == true){
+    else {
+        $id("Lunch1").disabled = true;
+        $id("Lunch2").disabled = true;
+        $id("chkLunch").disabled = true;
+    }
+}
+function LoadChange() {
+    if ($id("chkClassTeacher").checked == true) {
         $id("BtnChange").disabled = false;
     }
     else {
@@ -420,8 +419,8 @@ function LoadChange(){
     }
 }
 
-function LoadGroupTextBox(){
-    if ($id("chkStudent").checked == true){
+function LoadGroupTextBox() {
+    if ($id("chkStudent").checked == true) {
         $id("groupText1").disabled = false;
         $id("groupText2").disabled = false;
         $id("groupText3").disabled = false;
@@ -440,23 +439,22 @@ function LoadGroupTextBox(){
 }
 
 
-
 var Steps = {
-    view: function(step) {
-        if(step !== 'Finish') {
-            if(step === undefined)
+    view: function (step) {
+        if (step !== 'Finish') {
+            if (step === undefined)
                 step = "Step1";
-            $( "#stepsView" ).load( "../New/" + step + ".html" );
+            $("#stepsView").load("../New/" + step + ".html");
         }
         else {
-            $( "#stepsView" ).load( "../New/" + step + ".html", function(data) {
+            $("#stepsView").load("../New/" + step + ".html", function (data) {
                 Steps.showFullData();
             });
         }
         this.update();
         console.log("Content loaded");
     },
-    update: function() {
+    update: function () {
         this.data.school = $id('school');
         this.data.academic_year = $id('academic_year')
     },
@@ -467,21 +465,25 @@ var Steps = {
         periods: "",
         no_of_days: undefined
     },
-    showFullData: function() {
+    showFullData: function () {
         $('.finishContainer').append('' +
-            //'<div> First Step' + this.form.first + '</div>' +
-            //'<div> Second Step' + this.form.second + '</div>' +
-            //'<div> Third Step' + this.form.third + '</div>' +
-            //'<div> Fourth Step' + this.form.fourth + '</div>' +
-            '<div> Fourth Step Count: ' + this.form.fourth.tableData[0].count + '</div>' +
-            '<div> Fourth Step Name: ' + this.form.fourth.tableData[0].name + '</div>' +
-            '<div> Fourth Step Timeoff: ' + this.form.fourth.tableData[0].timeOff + '</div>' +
-            '<div> Fourth Step Short: ' + this.form.fourth.tableData[0].short + '</div>' +
-            '<div> Fifth Step Count: ' + this.form.five.tableData[0].count + '</div>' +
-            '<div> Fifth Step Name: ' + this.form.five.tableData[0].name + '</div>' +
-            '<div> Fifth Step Timeoff: ' + this.form.five.tableData[0].timeOff + '</div>' +
-            '<div> Fifth Step Short: ' + this.form.five.tableData[0].short + '</div>' +
-            '<div> Sixth Step: ' + this.form.six + '</div>'
+
+                '<div>First Step : ' + this.form.first.school + '</div>' +
+
+                '<div> Third Step Name: ' + this.form.third.tableData[0].name + '</div>' +
+                //      '<div> Third Step Timeoff: ' + this.form.third.tableData[0].timeOff + '</div>' +
+                //      '<div> Third Step Short: ' + this.form.third.tableData[0].short + '</div>' +
+                //      '<div> Third Step Count: ' + this.form.third.tableData[0].count + '</div>' +
+
+                '<div> Fourth Step Name: ' + this.form.fourth.tableData[0].name + '</div>' +
+                //      '<div> Fourth Step Timeoff: ' + this.form.fourth.tableData[0].timeOff + '</div>' +
+                //       '<div> Fourth Step Short: ' + this.form.fourth.tableData[0].short + '</div>' +
+                // '<div> Fifth Step Count: ' + this.form.five.tableData[0].count + '</div>' +
+                '<div> Fifth Step Name: ' + this.form.five.tableData[0].name + '</div>' +
+                //       '<div> Fifth Step Timeoff: ' + this.form.five.tableData[0].timeOff + '</div>' +
+                //       '<div> Fifth Step Short: ' + this.form.five.tableData[0].short + '</div>'
+                '<div> Sixth Step: ' + this.form.six.tableData[0].fullname + '</div> '
+            //   '<div> Sixth Step: ' + this.form.six.tableData[0].short + '</div> '
         );
     }
 };
@@ -490,86 +492,89 @@ Steps.view();
 
 
 Steps.form = {
-  first: {
-      school: "",
-      academic_year: "",
-      registration_name: "",
-      periods: "",
-      no_of_days: undefined,
-      weekend: "",
-      update: function(){
-          this.first.school = $id('school');
-          this.first.academic_year = $id('academic_year');
-          this.first.registration_name = $id('registration_name');
-          this.first.periods = $id('periods');
-          this.first.academic_year = $id('academic_year');
-          this.first.weekend = $id('weekend');
-      }
-  },
-  second: {
-      checks: [],
-      update: function() {
-      }
-  },
-  third: {
-      tableData: [],
-      update: function() {
-          var subjectData = [];
-          $('#subjectsEnter tbody tr').each(function(index, data){
-              var item = {};
-              var a = $(data).children();
-              item.name = a[0].innerText;
-              item.short = a[1].innerText;
-              item.count = a[2].innerText;
-              item.timeOff = a[3].innerText;
-              Steps.form.third.tableData.push(item);
-          });
-      }
-  },
-  fourth: {
-      tableData: [],
-      update: function() {
-          var subjectData = [];
-          $('#classesEnter tbody tr').each(function(index, data){
-              var item = {};
-              var a = $(data).children();
-              item.name = a[0].innerText;
-              item.short = a[1].innerText;
-              item.count = a[2].innerText;
-              item.timeOff = a[3].innerText;
-              Steps.form.fourth.tableData.push(item);
-          });
-      }
-  },
-  five: {
-      tableData: [],
-      update: function() {
-          var subjectData = [];
-          $('#classRoomEnter tbody tr').each(function(index, data){
-              var item = {};
-              var a = $(data).children();
-              item.name = a[0].innerText;
-              item.short = a[1].innerText;
-              item.count = a[2].innerText;
-              item.timeOff = a[3].innerText;
-              Steps.form.five.tableData.push(item);
-          });
-      }
-  },
-  six: {
-      tableData: [],
-      update: function() {
-          var subjectData = [];
-          $('#teacherEnter tbody tr').each(function(index, data){
-              var item = {};
-              var a = $(data).children();
-              item.name = a[0].innerText;
-              item.short = a[1].innerText;
-              item.count = a[2].innerText;
-              item.timeOff = a[3].innerText;
-              Steps.form.five.tableData.push(item);
-          });
-      }
-  }
+    update: function () {
+        this.first.school = $id('school').value;
+        this.first.academic_year = $id('academic_year');
+        this.first.registration_name = $id('registration_name');
+        this.first.periods = $id('periods');
+        this.first.academic_year = $id('academic_year');
+        this.first.weekend = $id('weekend');
+    },
+    first: {
+        school: "",
+        academic_year: "",
+        registration_name: "",
+        periods: "",
+        no_of_days: undefined,
+        weekend: ""
+    },
+    second: {
+        checks: [],
+        update: function () {
+        }
+    },
+
+    third: {
+        tableData: [],
+        update: function () {
+            var classData = [];
+            $('#subjectsEnter tbody tr').each(function (index, data) {
+                var item = {};
+                var a = $(data).children();
+                item.name = a[0].innerText;
+                item.short = a[1].innerText;
+                item.count = a[2].innerText;
+                item.timeOff = a[3].innerText;
+                Steps.form.third.tableData.push(item);
+            });
+        }
+    },
+
+    fourth: {
+        tableData: [],
+        update: function () {
+            var classData = [];
+            $('#classesEnter tbody tr').each(function (index, data) {
+                var item = {};
+                var a = $(data).children();
+                item.name = a[0].innerText;
+                item.short = a[1].innerText;
+                item.count = a[2].innerText;
+                item.timeOff = a[3].innerText;
+                Steps.form.fourth.tableData.push(item);
+            });
+        }
+    },
+
+    five: {
+        tableData: [],
+        update: function () {
+            var classroomsData = [];
+            $('#classRoomEnter tbody tr').each(function (index, data) {
+                var item = {};
+                var a = $(data).children();
+                item.name = a[0].innerText;
+                item.short = a[1].innerText;
+                item.count = a[2].innerText;
+                item.timeOff = a[3].innerText;
+                Steps.form.five.tableData.push(item);
+            });
+        }
+    },
+    six: {
+        tableData: [],
+        update: function () {
+            var teacherData = [];
+            $('#teacherEnter tbody tr').each(function (index, data) {
+                var item = {};
+                var a = $(data).children();
+                item.fullname = a[0].innerText;
+                item.short = a[1].innerText;
+                //item.count = a[2].innerText;
+                //item.timeOff = a[3].innerText;
+                Steps.form.six.tableData.push(item);
+            });
+        }
+    }
 };
 
